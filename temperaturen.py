2b3254dfab4d3b963f151d3065546300f99f08df
turndel <marko.dobric2000@gmail.com>
2026-03-16 19:27:22 +0100

    Added temperaturen.py function.
    A function that converts temperature from Fahrenheit to Celsius and vice versa. It also gives a boolean whether the current temperature is warm or cold.

---
+++ temperaturen.py
@@ -0,0 +1,63 @@
+# FUNKTION ZUR TEMPERATUR-UMRECHNUNG 
+
+# Rechnet Celsius in Fahrenheit um.
+def celsius_to_fahrenheit(celsius): 
+    
+    # Args:
+    #     Celsius: Temperatur in Celsius
+        
+    # Return:
+    #     Temperatur in Fahrenheit
+    
+    temp_fahr = (celsius * 9/5) + 32
+    print(f"{celsius:.2f}°C entspricht {temp_fahr:.2f}°F")
+    
+    return(temp_fahr)
+
+
+# Rechnet Fahrenheit in Celsius um.
+def fahrenheit_to_celsius(fahrenheit): 
+    
+    # Args:
+    #     Fahrenheit: Temperatur in Fahrenheit
+        
+    # Returns:
+    #     Temperatur in Celsius
+    
+    temp_cels = (fahrenheit - 32) * 5/9
+    print(f"{fahrenheit:.2f}°F entspricht {temp_cels:.2f}°C")
+    
+    return(temp_cels)
+
+
+# Prüft ob eine Temperatur als "warm" gilt (über 20°C).
+def ist_warm(temperatur, einheit="celsius"): 
+    
+    # Args:
+    #     Die Temperatur als Zahlenwert
+    #     Die Einheit: "celsius" oder "fahrenheit"
+        
+    # Returns:
+    #     True wenn Temperatur warm, ansonsten False
+    
+    # Ist die Einheit in Fahrenheit, soll sie zu Celsius umgewandelt werden.
+    if einheit == "fahrenheit":
+        temperatur_celsius = fahrenheit_to_celsius(temperatur)
+    else:
+        temperatur_celsius = temperatur
+    
+    # Ist die Temperatur zu hoch, oder unterhalb der Grenze?
+    if temperatur_celsius > 20:
+        
+        print(f"{temperatur_celsius:.2f}°C ist warm. Ich brauch Wasser.")
+        return True
+    
+    elif temperatur_celsius < 20 and temperatur_celsius > 0:
+        
+        print(f"{temperatur_celsius:.2f}°C ist nicht warm. Kein Wasser nötig.")
+        return False
+    
+    elif temperatur_celsius < 0:
+        
+        print(f"{temperatur_celsius:.2f}°C ist kalt. Brrrrr!")
+        return False
