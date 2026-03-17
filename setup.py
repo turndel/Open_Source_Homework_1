4b8f752d33b9554e48ede23c7395f0e7121609f1
turndel <marko.dobric2000@gmail.com>
2026-03-17 09:44:53 +0100

    Added src and corrections.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="temperatur_projekt",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
+    py_modules=["temperaturen"],
     python_requires=">=3.9",
 )
--- tests/test_temperaturen.py
+++ tests/test_temperaturen.py
@@ -1,31 +1,37 @@
 # Test-Code zur Überprüfung des Codes
+import sys, os
 
+# Füge den Hauptordner zum Python-Pfad hinzu
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.dein_projekt.temperaturen import (
+# Jetzt kann importiert werden
+from src.temperaturen.temperaturen import (
     celsius_to_fahrenheit, 
     fahrenheit_to_celsius,
     ist_warm
 )
 
-
+# Testen der jeweiligen Funktionen
 def test_celsius_to_fahrenheit():
 
-    """Testet die Celsius->Fahrenheit Umrechnung."""
+    """ Testet die Celsius->Fahrenheit Umrechnung. """
     assert celsius_to_fahrenheit(0) == 32
     assert celsius_to_fahrenheit(100) == 212
     assert round(celsius_to_fahrenheit(20), 2) == 68.00
 
+
 def test_fahrenheit_to_celsius():
 
-    """Testet die Fahrenheit->Celsius Umrechnung."""
+    """ Testet die Fahrenheit->Celsius Umrechnung. """
     assert fahrenheit_to_celsius(32) == 0
     assert fahrenheit_to_celsius(212) == 100
     assert round(fahrenheit_to_celsius(68), 2) == 20.00
 
 
+
 def test_ist_warm():
     
-    """Testet die Warm-Erkennung."""
+    """ Testet die Warm-Erkennung. """
     assert ist_warm(25) == True   # Warm
     assert ist_warm(20) == False  # Grenze: nicht warm
     
